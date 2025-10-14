7ba5dbec18a3c1cd3f84f35f91c9a4a0317eb129
matthewr95 <73726762+matthewr95@users.noreply.github.com>
2025-10-14 14:22:26 -0400

    Create connect.py

---
+++ connect.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import time
+
+serverQuestion = """
+Enter the number for the kind of connection you want to establish:
+1. Automation Server
+2. Cloud Connect Server
+3. Forwarder 3
+4. Forwarder 4
+0. Exit
+
+Option: """
